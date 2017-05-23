e8818521961bb42339bee3071bf807bde5df7c37
Christina McDonnell <christinamcdonnell@ymail.com>
2017-05-22 19:58:17 -0500

    Third Commit for cheese-mvc. CheeseController.java  Working thru Intro to Spring Boot Views Part 2.

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -1,72 +1,45 @@
-package org.launchcode.controllers;
+package org.launchcode.cheesemvc.controllers;
 
-import org.launchcode.models.Cheese;
-import org.launchcode.models.CheeseData;
-import org.launchcode.models.CheeseType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
+import java.text.AttributedString;
 import java.util.ArrayList;
 
 /**
- * Created by LaunchCode
+ * Created by Christy on 5/18/2017.
  */
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
+    static ArrayList<String> cheeses = new ArrayList<>();
 
-    // Request path: /cheese
-    @RequestMapping(value = "")
+    // Request path /cheese because of RequestMapping above for the entire controller pkg
+    @RequestMapping(value="")
     public String index(Model model) {
 
-        model.addAttribute("cheeses", CheeseData.getAll());
+        model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
 
         return "cheese/index";
     }
 
-    @RequestMapping(value = "add", method = RequestMethod.GET)
+    @RequestMapping(value = "add", method=RequestMethod.GET)
     public String displayAddCheeseForm(Model model) {
+        //AttributedString model;
         model.addAttribute("title", "Add Cheese");
-        model.addAttribute(new Cheese());
-        model.addAttribute("cheeseTypes", CheeseType.values());
         return "cheese/add";
     }
 
-    @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@ModelAttribute  @Valid Cheese newCheese,
-                                       Errors errors, Model model) {
-
-        if (errors.hasErrors()) {
-            model.addAttribute("title", "Add Cheese");
-            return "cheese/add";
-        }
-
-        CheeseData.add(newCheese);
+    @RequestMapping(value="add", method=RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName) {
+        cheeses.add(cheeseName);
+        // redirect to /cheese
         return "redirect:";
     }
-
-    @RequestMapping(value = "remove", method = RequestMethod.GET)
-    public String displayRemoveCheeseForm(Model model) {
-        model.addAttribute("cheeses", CheeseData.getAll());
-        model.addAttribute("title", "Remove Cheese");
-        return "cheese/remove";
-    }
-
-    @RequestMapping(value = "remove", method = RequestMethod.POST)
-    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds) {
-
-        for (int cheeseId : cheeseIds) {
-            CheeseData.remove(cheeseId);
-        }
-
-        return "redirect:";
-    }
-
 }
+
